97e0a2b812f939a92fb637cff49cd658e3b80985
michael.hedrick@ucdenver.edu <michael.hedrick@ucdenver.edu>
2018-02-18 17:18:06 -0700

    ??

---
+++ auth.js
@@ -0,0 +1,20 @@
+// Load required packages
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+passport.use(new BasicStrategy(
+    function(username, password, done) {
+        //hard coded
+        var user = { name: "hedrickm" };
+        if (username == user.name && password == "12345")
+        {
+            return done(null, user);
+        }
+        else
+        {
+            return done(null, false);
+        }
+    }
+));
+
+exports.isAuthenticated = passport.authenticate('basic', { session : false });
