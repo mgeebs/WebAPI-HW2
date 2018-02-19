f94a55c0fad6afeacaa9d8a9c44684d430a8aca6
michael.hedrick@ucdenver.edu <michael.hedrick@ucdenver.edu>
2018-02-19 16:19:42 -0700

    all routes returning headers, query, and unique_key basic auth strategy working in delete route

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var authController = require('./auth');
-var http = require('http');
+//var authController = require('./auth');
 var app = express();
 var uniqueKey = process.env.UNIQUE_KEY;
 
@@ -21,10 +20,12 @@ router.post('/posts', function(req,res) {
     if(Object.keys(req.query).length === 0) {
         res.status(400).send("The query was empty");
     }
+    if(Object.keys(req.headers).length === 0) {
+        res.status(400).send("There were no headers");
+    }
     else {
         res.status(200).send(postObject); //send the query and headers back in a response
     }
-
 });
 
 router.get('/gets', function(req,res) {
@@ -33,6 +34,9 @@ router.get('/gets', function(req,res) {
 
     if(Object.keys(req.query).length === 0) {
         res.status(400).send("The query was empty");
+    }
+    if(Object.keys(req.headers).length === 0) {
+        res.status(400).send("There were no headers");
     }
     else {
         res.status(200).send(getObject); //send the query and headers back in a response
@@ -46,6 +50,9 @@ router.put('/puts', function(req,res) {
     if(Object.keys(req.query).length === 0) {
         res.status(400).send("The query was empty");
     }
+    if(Object.keys(req.headers).length === 0) {
+        res.status(400).send("There were no headers");
+    }
     else {
         res.status(200).send(putObject); //send the query and headers back in a response
     }
@@ -53,15 +60,18 @@ router.put('/puts', function(req,res) {
 
 // delete route uses passport basic auth strategy
 router.delete('/deletes', passport.authenticate('basic', { session: false }), function(req,res) {
-        //Create an object containing the query, header, and environment variable
-        var deleteObject = {query: req.query, headers: req.headers, unique_key: uniqueKey}
+    //Create an object containing the query, header, and environment variable
+    var deleteObject = {query: req.query, headers: req.headers, unique_key: uniqueKey}
 
-        if (Object.keys(req.query).length === 0) {
-            res.status(400).send("The query was empty");
-        }
-        else {
-            res.status(200).send(deleteObject); //send the query and headers back in a response
-        }
+    if (Object.keys(req.query).length === 0) {
+        res.status(400).send("The query was empty");
+    }
+    if(Object.keys(req.headers).length === 0) {
+        res.status(400).send("There were no headers");
+    }
+    else {
+        res.status(200).send(deleteObject); //send the query and headers back in a response
+    }
 });
 
 app.use('/', router);
